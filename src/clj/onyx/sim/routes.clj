83cc4fcfa7ba11d0d50be4a69d13c039c6516d82
Brian Marco <brian@mademonkey.com>
2023-01-31 11:09:20 -0500

    make more friendly to proxy-ing

--- src/clj/onyx/sim/routes.clj
+++ src/clj/onyx/sim/routes.clj
@@ -2,7 +2,7 @@
   (:require [taoensso.timbre :as log]
             [compojure.core :refer [ANY GET PUT POST DELETE routes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [content-type resource-response response]]))
+            [ring.util.response :refer [content-type resource-response redirect response]]))
 
 (defn home-routes [endpoint]
   (log/info "example" (->
@@ -10,9 +10,10 @@
          (resource-response  {:root "public"})
          (content-type "text/html")))
   (routes
-   (GET "/" _
+   (GET "/" _ (redirect "onyx-sim"))
+   (GET "/onyx-sim" _
         (->
-         "index.html"
+         "onyx/sim/index.html"
          (resource-response  {:root "public"})
          (content-type "text/html")))
     (GET "/favicon.ico" _
